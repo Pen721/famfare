56f0f10de3af4f43840497902cfc784495bb887c
Penny <pennyb@mit.edu>
2022-10-22 15:14:21 -0400

    crisis averted yayyy

--- my-app/src/config.js
+++ my-app/src/config.js
@@ -1,17 +1,22 @@
-import { initializeApp} from 'firebase/app';
-import { getDatabase } from "firebase/database";
-import { doc, setDoc } from "firebase/firestore"; 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAuth } from "firebase/auth";
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBfrTbj3-67E1OasTPDgH_LjV1xsvFmzSE",
-    authDomain: "famfare2-d16cd.firebaseapp.com",
-    projectId: "famfare2-d16cd",
-    storageBucket: "famfare2-d16cd.appspot.com",
-    messagingSenderId: "706768556538",
-    appId: "1:706768556538:web:a7815353328ad67c0e1443",
-    URL: 'famfare2-d16cd.firebaseio.com',
+  apiKey: "AIzaSyATtOVHpg9kM327jKrKPb9hMZp21N9rOKI",
+  authDomain: "fir-auth-4c8a7.firebaseapp.com",
+  projectId: "fir-auth-4c8a7",
+  storageBucket: "fir-auth-4c8a7.appspot.com",
+  messagingSenderId: "882135999227",
+  appId: "1:882135999227:web:dcba71d88a017f4489c65e"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
-// export const db = app.database();
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
